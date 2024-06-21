7d712cc3ba9a38ff92e91a7c8fff5b260dfd6d44
Mariana Dias <marianamkt6@gmail.com>
2024-06-21 10:49:14 -0300

    Adiciona o arquivo Router.tsx e outras mudanças

--- ProjetoPomodoro/src/App.tsx
+++ ProjetoPomodoro/src/App.tsx
@@ -1,22 +1,21 @@
 import { ThemeProvider } from 'styled-components'
-import { Buttton } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GoblalStyle } from './styles/global'
+ 
+ 
+
 
 export function App() {
   
   return (
- 
-<ThemeProvider theme={defaultTheme}>
-<Buttton variant="primary"  />
-<Buttton variant="secondary" />
-<Buttton variant="success" />
-<Buttton variant="danger" />
-<Buttton/>
+    <ThemeProvider theme={defaultTheme}>
 
-<GoblalStyle/>
+   
+
+    <GoblalStyle/>
     </ThemeProvider>
- 
+     
+
   )
 }
  
