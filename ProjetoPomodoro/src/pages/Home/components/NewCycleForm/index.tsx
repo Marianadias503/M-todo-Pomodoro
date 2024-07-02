4192948ff8cabe980939c0d17b063e3be572c4ca
Mariana Dias <marianamkt6@gmail.com>
2024-07-02 16:40:11 -0300

    separando os componentes

---
+++ ProjetoPomodoro/src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,36 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './style';
+
+
+export function NewCycleForm(){
+    return(
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
+            {...register('task')}
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto1"></option>
+            <option value="Projeto2"></option>
+            <option value="Projeto3"></option>
+            <option value="Projeto4"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            disabled={!!activeCycle}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+    )
+}
--- ProjetoPomodoro/src/pages/Home/index.tsx
+++ ProjetoPomodoro/src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
 import { useEffect, useState } from 'react';
 import { differenceInSeconds } from 'date-fns';
+import { NewCycleForm } from "./components/NewCycleForm";
+import { Countdown } from "./components/Countdown";
 
 interface Cycle {
   id: string;
@@ -105,7 +107,7 @@ function handleInterruptCycle(){
    setActiveCycleId(null)
 
 }
-  
+   
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
   const minutesAmount = Math.floor(currentSeconds / 60);
   const secondsAmount = currentSeconds % 60;
@@ -125,43 +127,10 @@ function handleInterruptCycle(){
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-        <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            id="task"
-            list="task-suggestions"
-            placeholder="Dê um nome para o seu projeto"
-            disabled={!!activeCycle}
-            {...register('task')}
-          />
-          <datalist id="task-suggestions">
-            <option value="Projeto1"></option>
-            <option value="Projeto2"></option>
-            <option value="Projeto3"></option>
-            <option value="Projeto4"></option>
-          </datalist>
+      <NewCycleForm />
+      <Countdown/>
 
-          <label htmlFor="minutesAmount">Durante</label>
-          <MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            placeholder="00"
-            step={5}
-            min={5}
-            max={60}
-            disabled={!!activeCycle}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
-          <span>minutos.</span>
-        </FormContainer>
-
-        <CountdownContainer>
-          <span>{minutes[0]}</span>
-          <span>{minutes[1]}</span>
-          <Separator>:</Separator>
-          <span>{seconds[0]}</span>
-          <span>{seconds[1]}</span>
-        </CountdownContainer>
+      
 
         {activeCycle ? (
           <StopCountDownButton onClick={handleInterruptCycle} type="button">
