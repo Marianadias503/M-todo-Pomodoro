2d17da119a54ee2dfb39fc6fd3093e1c01d90969
Mariana Dias <marianamkt6@gmail.com>
2024-07-10 10:30:26 -0300

    adicionando a visiblilidade do cycle, no histórico

--- ProjetoPomodoro/src/contexts/CyclesContext.tsx
+++ ProjetoPomodoro/src/contexts/CyclesContext.tsx
@@ -15,6 +15,7 @@ interface CreateCycleData {
 }
 
 interface CyclesContextType {
+  cycles: Cycle []
   activeCycle: Cycle | undefined;
   activeCycleId: string | null;
   markCurrentCycleAsFinished: () => void;
@@ -81,6 +82,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         markCurrentCycleAsFinished,
--- ProjetoPomodoro/src/pages/History/index.tsx
+++ ProjetoPomodoro/src/pages/History/index.tsx
@@ -3,7 +3,7 @@ import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/CyclesContext";
 
 export function History() {
-    const { cycles } = useContext(CyclesContext);
+    const { cycles } = useContext(CyclesContext)
 
     return (
         <HistoryContainer>
